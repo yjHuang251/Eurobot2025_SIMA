8e8208074c498555af25637e6861f10e0310488a
ginga931010 <axyz50413@gmail>
2025-01-06 14:25:47 +0800

    vl53

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 #include <Arduino.h>
 #include "Wheel.h"
 #include "Line.h"
-
+#include "VL53L0X.h"
 Wheel L_wheel(2/*control*/, 0/*feedback*/);
 Wheel R_wheel(15, 0);
 Line line(13, 12, 14, 27, 26);
-
+VL53L0X sensor;
 // time_t run_time;
 const int v_0=20;
 
@@ -17,13 +17,32 @@ void setup() {
   // line.init();
   L_wheel.control(v_0);
   R_wheel.control(v_0);
+  Wire.begin();  // 啟動I2C通訊
+
+  sensor.setTimeout(500);  // 設定感測器超時時間
+  if (!sensor.init()) {
+    Serial.println("Failed to detect and initialize sensor!");
+    while (1) {}
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  // 在序列埠監控視窗顯示測距值
+  Serial.print(sensor.readRangeSingleMillimeters());
+  // 若發生超時（感測器沒有回應），則顯示“TIMEOUT”。
+  if(sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
+  if(sensor.readRangeSingleMillimeters() <= 150){
+    
+    L_wheel.control(0);
+    R_wheel.control(0);
+  
+  }else{
+  Serial.println();
   line.PIDcontrol();
   L_wheel.control(-1*(v_0*line.getLspd()));
   R_wheel.control(v_0*line.getRspd());
+  }
   /*for(int i=0;i<5;i++){
     Serial.print(line.val[i]);
     Serial.print(" ");
