d71b928c4139458905a87a7fecbbdc1bf1d5a2cc
yuhsiang1117 <seanchen1117@gmail.com>
2025-01-20 18:13:43 +0800

    add pid(not test)

--- lib/Line_Fol/Line.h
+++ lib/Line_Fol/Line.h
@@ -1,28 +1,29 @@
-
+#ifndef LINE_H
+#define LINE_H
 #include <Arduino.h>
+#include <QTRSensors.h>
+#include <vector>
+#define SensorCount 8
 
-#define K_P 10
-#define K_D 0.1
-
-typedef class Line{
+typedef class LINE{
     public:
-    Line(uint8_t, uint8_t, uint8_t, uint8_t, uint8_t);
-    void init();
-    void PIDcontrol();
-    int getLspd();
-    int getRspd();
-    int val[5]={0};
-    bool stop = false;
-    int lastblack;
-    int last_lastblack;
-    
+        LINE (std::vector<uint16_t> pins) : LMspeed(0), RMspeed(0){
+            uint16_t cnt = 0;
+            for(auto i : pins){
+                sensorValues[cnt] = (const uint16_t)i;
+                cnt++;
+            }
+        }
+        void init();
+        void PIDcontrol(const double &KP, const double &KD, const double &v0);
+        void print();
+        double getLspd();
+        double getRspd();
+        ~LINE(){free(sensorValues);}
     private:
-    int l_spd=0;
-    int r_spd=0;
-    int error_now=0;
-    int error_last=0;
-    int d_error=0;
-    int threshold[5]={400, 400, 400, 400, 400};
-    uint8_t pins[5]={0};
-    
+        QTRSensors qtr;
+        uint16_t sensorValues[SensorCount];
+        double LMspeed, RMspeed;
 }Line;
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -4,61 +4,27 @@
 
 Wheel L_wheel(2/*control*/, 0/*feedback*/);
 Wheel R_wheel(15, 0);
-Line line(13, 35, 14, 27, 26);
+Line line({13, 35, 14, 27, 26});
 
 // time_t run_time;
 const double v_0=30;
-const double v_rst = 30;
-
-double v_0r = 50;
-double v_0l = 45;
-const double Kpl = 5.0;
-const double Kpr = 1.0;
+const double KP = 1.0;
+const double KD = 1.0;
 
 // put function declarations here:
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  // line.init();
+  line.init();
   L_wheel.control(v_0);
   R_wheel.control(v_0);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  line.PIDcontrol();
-  //Serial.println(line.stop);
-  if(line.stop){
-    L_wheel.control(0);
-    R_wheel.control(0);
-  }
-  else{
-    //Serial.println("go");
-    if(line.getLspd()==1 ) v_0l += (110-v_0l)/2;
-    if(line.getRspd()==1) v_0l += 60.0;
-    if(v_0l>100) v_0l=100;
-    //if(line.getRspd()==1) v_0r = v_rst;
-    L_wheel.control(-1*(v_0l));
-    R_wheel.control(v_0r);
-    /*Serial.print(v_0+v_0l);
-    Serial.print(" ");
-    Serial.print(v_0+v_0r);
-    Serial.print(" ");
-    Serial.println();*/
-    v_0l -= Kpl;
-    v_0l = max(40.0,v_0l);
-    //v_0r -= Kpr;
-    v_0r = max(0.0,v_0r);
-  }
-  /*for(int i=0;i<5;i++){
-    Serial.print(line.val[i]);
-    Serial.print(" ");
-  }
-  Serial.println();*/
-  // Serial.print("left: ");
-  // Serial.println(v_0+line.getLspd());
-  // Serial.print("right: ");
-  // Serial.println(v_0+line.getRspd());
+  line.PIDcontrol(KP, KD, v_0);
+  L_wheel.control(line.getLspd());
+  R_wheel.control(line.getRspd());
 } 
 // put function definitions here:
